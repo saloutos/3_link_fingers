dc874347c6c2d7193477fcadb2a07a61caf078aa
Andrew SaLoutos <saloutos96@gmail.com>
2022-03-28 17:26:55 +0000

    cleaning up

--- main.cpp
+++ main.cpp
@@ -9,17 +9,6 @@ DigitalOut led(LED1);
 
 I2C i2c1(PF_0, PF_1); //(PB_9, PB_8); // SDA, SCL
 I2C i2c2(PD_13, PD_12); 
-// SPI spi(PE_6, PE_5, PE_2); // MOSI, MISO, SCK
-// DigitalOut cs(PE_4);
-
-// variables for encoder -> distance calculation
-// float counts_to_mm = 3.14159f*24.257f/4096.0f;
-// float revs_to_mm = 3.14159f*24.257f;
-// float starting_position = 0.0f;
-// int starting_counts = 0;
-// int num_revolutions = 0;
-// int new_enc_pos = 0;
-// int old_enc_pos = 0;
 
 // initialize sensors
 VL6180X tof1;
@@ -30,11 +19,7 @@ VL6180X tof5;
 VL6180X tof6;
 int range[6];
 int range_status[6];
-
-// data variables
 uint8_t MUX_ADDR = (0x70<<1);
-// int prox[3];
-// float position = 0;
 
 Timer t;
 float loop_time = 0.005; // 200hz is probably maximum sample rate since that is pressure sensor rate too
@@ -69,12 +54,8 @@ int main() {
     
     i2c1.frequency(400000); // set bus freq
     i2c2.frequency(400000); 
-
-    // spi.frequency(1000000); // set bus freq
-    // spi.format(16,0); // try 12 bits? sensor is 12 bits, SPI mode 2 (inverted clock, sampled on falling edge)
     
     // perform any setup for the sensor here
-    // TODO: call load setting function, start continuous ranging
     pc.printf("Sensor 1...\n\r");
     set_mux1(2); // channel 2 on mux 
     wait_us(100);
@@ -147,13 +128,7 @@ int main() {
     t2.reset();
     t2.start();
     t3.reset();
-    t3.start();
-//    cs = 0;
-//    starting_counts = spi.write(0xFFFF);
-//    cs = 1;
-//    starting_counts = (starting_counts>>3);
-//    starting_position = ((float)starting_counts)*counts_to_mm;  
-    
+    t3.start(); 
 
     pc.printf("Starting...\n\r");
     while (1) {
@@ -204,28 +179,7 @@ int main() {
         wait_us(10);
 
         samp2 = t2.read_us();
-//        t2.reset();
-//        light = tof.readLux(VL6180X_ALS_GAIN_5);
-//        samp2 = t2.read_us();
-        
-        
-    //    old_enc_pos = new_enc_pos;
-    //    cs = 0;
-    //    new_enc_pos = spi.write(0xFFFF);
-    //    cs = 1;
-    //    new_enc_pos = (new_enc_pos>>3);
-    //    if ((new_enc_pos>3000)&&(old_enc_pos<1000)){
-    //        num_revolutions -= 1;
-    //    } else if ((new_enc_pos<1000)&&(old_enc_pos>3000)){
-    //        num_revolutions += 1;
-    //    }
-    //    position = -(((float)new_enc_pos)*counts_to_mm + ((float)num_revolutions)*revs_to_mm - starting_position);
-        
-        // print data
-        // pc.printf("%f, %d\n\r", t3.read(), range);
-//        pc.printf("%d, %f, %d, %d\n\r", range, light, samp1, samp2);
-//        pc.printf("prox: %d, raw_enc: %d, pos: %.2f\n\r", prox, new_enc_pos, position); //new_enc_pos); //(enc_data>>3));
-        // pc.printf("%f, %d, %.2f\n\r", t3.read(), range, position);
+
         t2.reset();
         // printing data takes about 900us at baud of 460800
         // pc.printf("%.2f, %d, %d, %d\n\r",t3.read(), samp1, samp2, samp3);
